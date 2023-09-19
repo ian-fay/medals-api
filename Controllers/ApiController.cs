9c62423b9311958c5c4a593b772532bc1e09f643
Ian Fay <ifay@my.wctc.edu>
2023-09-19 07:30:00 -0500

    Changes made to make API functional

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Medals.Models;
+
+namespace Medals.Controllers
+{
+    [ApiController, Route("[controller]/country")]
+    public class ApiController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ApiController(DataContext db)
+        {
+            _dataContext = db;
+        }
+        // http get entire collection
+        [HttpGet, SwaggerOperation(summary: "return entire collection", null)]
+        public IEnumerable<Country> Get()
+        {
+            return _dataContext.Countries;
+        }
+
+        // http get specific member of collection
+        [HttpGet("{id}"), SwaggerOperation(summary: "return specific member of collection", null)]
+        public Country Get(int id)
+        {
+            return _dataContext.Countries.Find(id);
+        }
+
+        // http post member to collection
+        [HttpPost, SwaggerOperation(summary: "add member to collection", null), ProducesResponseType(typeof(Country), 201), SwaggerResponse(201, "Created")]
+        public async Task<ActionResult<Country>> Post([FromBody] Country country) {
+            _dataContext.Add(country);
+            await _dataContext.SaveChangesAsync();
+            return country;
+        }
+
+        // http delete member from collection
+        [HttpDelete("{id}"), SwaggerOperation(summary: "delete member from collection", null), ProducesResponseType(typeof(Country), 204), SwaggerResponse(204, "No Content")]
+        public async Task<ActionResult> Delete(int id){
+            Country country = await _dataContext.Countries.FindAsync(id);
+            if (country == null){
+                return NotFound();
+            }
+            _dataContext.Remove(country);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Medals.Models;
+
+// Connection info stored in appsettings.json
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Register the DataContext service
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(configuration["ConnectionStrings:DefaultSQLiteConnection"]));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Open",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "Medals API", 
+        Version = "v1",
+        Description = "Olympic Medals API",
+    });
+     c.EnableAnnotations();
+    c.TagActionsBy(api => new[] { api.HttpMethod });
+});
 
 var app = builder.Build();
 
+app.UseCors("Open");
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
